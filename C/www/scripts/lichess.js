6f8baaa2750f6ff406473fe8c5d43f117c6a756b
Lanyon <lanyon@handly.co>
2016-11-28 16:39:36 -0600

    anonymous ai play

--- C/www/scripts/lichess.js
+++ C/www/scripts/lichess.js
@@ -20,7 +20,13 @@ function createGame() {
     xhttp.setRequestHeader("Accept", "application/vnd.lichess.v1+json");
     xhttp.send(params);
 
-    loadUser();
+    alert(xhttp.status);
+    alert(xhttp.getResponseHeader("Location"));
+    alert(xhttp.responseText);
+
+    gameConnectAnon(JSON.parse(xhttp.responseText).url.socket, JSON.parse(xhttp.responseText).player.version);
+
+    //loadUser();
 }
 
 function loadUser() {
@@ -42,6 +48,18 @@ function loadUser() {
     }
 
     document.getElementById("gameConnectButton").disabled = false;
+}
+
+function loadLichess() {
+    var xhttp = new XMLHttpRequest();
+    var url = "http://en.lichess.org/";
+    xhttp.open("GET", url, false);
+
+    // send the proper header information along with the request
+    xhttp.setRequestHeader("Accept", "application/vnd.lichess.v1+json");
+    xhttp.send();
+
+    console.log(xhttp.responseText);
 }
 
 function loadLogout() {
@@ -138,6 +156,178 @@ function fetchFEN(fullID) {
 function updateVersion(version) {
     window.version = version;
     //alert("version is now: " + version);
+}
+
+// connect to lobby
+function lobbyConnect() {
+
+
+    clientId = Math.random().toString(36).substring(2); // created and stored by the client
+
+    var socketUrl = 'wss://socket.lichess.org/lobby/socket/v1?sri=' + clientId;
+    //alert(socketUrl);
+
+    window.version = 0;
+
+
+    window.socket = new WebSocket(socketUrl);
+
+    socket.onopen = function () {
+
+        window.pinger = setInterval(function () {
+
+            socket.send(JSON.stringify({
+                t: 'p',
+                v: version
+            }));
+
+        }, 2000)
+
+
+    };
+
+    socket.onmessage = function (event) {
+
+        console.log(event.data);
+        
+
+    };
+
+    socket.onerror = function () {
+        console.log('error occurred!');
+    };
+
+    socket.onclose = function (event) {
+        console.log("socket closed");
+
+        clearInterval(pinger);
+
+    };
+
+}
+
+// connect to a game as a player
+function gameConnectAnon(baseUrl,version) {
+    window.awaitingAck = false;
+    window.winner = false;
+
+    window.version = version;
+
+    clientId = Math.random().toString(36).substring(2); // created and stored by the client
+
+    var socketUrl = 'ws://socket.en.lichess.org:9021' + baseUrl + '?sri=' + clientId + '&ran=--ranph--';
+    //alert(socketUrl);
+
+    try {
+        socket.close();
+    }
+    catch (err) {
+
+    }
+
+    window.socket = new WebSocket(socketUrl);
+
+    socket.onopen = function () {
+
+        window.pinger = setInterval(function () {
+
+            socket.send(JSON.stringify({
+                t: 'p',
+                v: version
+            }));
+
+        }, 2000)
+
+        //setTimeout(function () { alert('connected!'); }, 1);
+        syncFEN();
+
+
+    };
+
+    socket.onmessage = function (event) {
+        //alert("I received a message!");
+
+        var currEvent = event;
+        var eventData = JSON.parse(currEvent.data);
+        if (eventData.hasOwnProperty("t")) {
+            //alert(eventData.d.uci);
+            if (eventData.t == "resync") {
+                gameConnect();
+                //setTimeout(function () { alert("game resynced and connected!"); }, 1);
+
+            }
+            if (awaitingAck && eventData.t != "ack") {
+                sendMove();
+            }
+            else if (awaitingAck && eventData.t == "ack") {
+                //alert("ack received");
+                awaitingAck = false;
+            }
+            if (eventData.t == "move") {
+                board.move(eventData.d.uci.substring(0, 2) + "-" + eventData.d.uci.substring(2, 4));
+                bluetoothSerial.write(eventData.d.uci);
+            }
+            else if (eventData.t == "b") {
+                if (eventData.d[(eventData.d.length) - 1].t == "end") {
+                    board.move(eventData.d[(eventData.d.length) - 2].d.uci.substring(0, 2) + "-" + (eventData.d[(eventData.d.length) - 2].d.uci.substring(2, 4)));
+                    bluetoothSerial.write(eventData.d[(eventData.d.length) - 2].d.uci);
+                    //switch the below to a getWinner function later on (can retreive info about finished games on lichess)
+                    if (!window.winner) {
+                        window.winner = eventData.d[(eventData.d.length) - 2].d.winner;
+                        setTimeout(function () { alert(winner + " wins!"); }, 1000);
+                    }
+                }
+            }
+        }
+
+
+
+        //    if (currcurrEvent.d.uci) {
+        //        updateVersion(currEvent.d.uci);
+        //        if (document.getElementById('autoSend').checked) {
+        //            console.log(currEvent.d.uci.substring(0, 2) + "-" + currEvent.d.uci.substring(2, 4));
+        //            board.move(currEvent.d.uci.substring(0, 2) + "-" + currEvent.d.uci.substring(2, 4));
+        //            bluetoothSerial.write(currEvent.d.uci);
+        //        }
+        //        document.getElementById("opponentMove").value = currEvent.d.uci;
+        //        //if (d.castle)
+        //        //    alert("castle!");
+        //        //if (d.enpassant)
+        //        //    alert("en passant!");
+        //        if (d.check)
+        //            alert("check!");
+        //    }
+        //    else if (currEvent.d[1].t == "end") {
+        //        if (document.getElementById('autoSend').checked) {
+        //            console.log(currEvent.d[0].uci.substring(0, 2) + "-" + currEvent.d[0].uci.substring(2, 4));
+        //            board.move(currEvent.d[0].uci.substring(0, 2) + "-" + currEvent.d[0].uci.substring(2, 4));
+        //            bluetoothSerial.write(currEvent.d[0].d.uci);
+        //        }
+        //        document.getElementById("opponentMove").value = currEvent.d[0].d.uci;
+        //        if (d[1].d)
+        //            alert(d[1].d + "wins!");
+        //        else
+        //            alert("stalemate!");
+        //    }
+
+
+
+
+
+    };
+
+    socket.onerror = function () {
+        alert('error occurred!');
+    };
+
+    socket.onclose = function (event) {
+        //setTimeout(function () { alert('connection lost! Please Reconnect to game'); }, 1);
+
+        clearInterval(pinger);
+        //socket.close();
+        //gameConnect();
+    };
+
 }
 
 // connect to a game as a player
